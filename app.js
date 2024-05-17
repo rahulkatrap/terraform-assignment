6d0a502fa50679147f14e03ae7f3c24bb8a211cf
Ubuntu <ubuntu@ip-172-31-12-162.ap-south-1.compute.internal>
2024-05-17 11:44:37 +0000

    app

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
